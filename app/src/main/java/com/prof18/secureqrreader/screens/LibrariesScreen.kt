65a82ae422c67884e59e3d3bc24da59e96511053
Marco Gomiero <marco.gomiero.93@gmail.com>
2026-07-12 16:30:52 +0200

    Remove obsolete illustration resources

--- app/src/main/java/com/prof18/secureqrreader/screens/LibrariesScreen.kt
+++ app/src/main/java/com/prof18/secureqrreader/screens/LibrariesScreen.kt
@@ -23,20 +23,14 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.displayCutout
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import com.mikepenz.aboutlibraries.ui.compose.LibrariesContainer
 import com.prof18.secureqrreader.components.LibrariesScreenScaffold
-import com.prof18.secureqrreader.style.Margins
 import com.prof18.secureqrreader.style.SecureQrReaderTheme
 
 @Composable
@@ -59,16 +53,6 @@ fun LibrariesScreen(
                     .padding(paddingValues)
                     .padding(padding)
             ) {
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(Margins.regular)
-                        .wrapContentWidth(align = Alignment.CenterHorizontally),
-                    text = "Vectors from freepik - it.freepik.com",
-                    style = MaterialTheme.typography.body1,
-                    color = MaterialTheme.colors.onBackground,
-                )
-
                 LibrariesContainer(
                     Modifier.fillMaxSize()
                         .navigationBarsPadding()
